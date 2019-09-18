a0c30cd4920abac04abf142e02f732d0e5e470e1
marcus-sa <marcus-sa@outlook.com>
2019-09-19 00:30:05 +0200

    feat(paths): set default for qt_pga_platform

--- export_paths.sh
+++ export_paths.sh
@@ -4,4 +4,5 @@ cat >> ~/.bashrc <<EOL
 export QMAKE_PATH=/usr/lib/x86_64-linux-gnu/qt5/bin/qmake
 export QT_INCLUDE_DIRS=/usr/include/x86_64-linux-gnu/qt5
 export QT_LIBRARIES=/usr/lib/x86_64-linux-gnu
+export QT_QPA_PLATFORM=offscreen
 EOL
